70bdc35d56539875a07794aa021b82231b134dc2
codelingo <hello@codelingo.io>
2017-10-28 05:27:19 +1300

    first commit

---
+++ count.py
@@ -0,0 +1,15 @@
+# Python 3
+
+def main():
+	count = 1
+	# Code block 1
+	while count < 11:
+	    print(count)
+	    count = count + 1
+	# Code block 2
+	if count == 11:
+	    print('Counting complete.')
+
+
+if __name__ == "__main__": main()
+
